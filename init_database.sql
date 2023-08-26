6fa65925eff87cc60b940aa6b5334d83ac2a75b2
ilyayashkin <drum1lord@gmail.com>
2023-08-26 22:10:41 +0800

    db tables

--- init_database.sql
+++ init_database.sql
@@ -2,22 +2,28 @@ DROP SCHEMA public CASCADE;
 CREATE SCHEMA public;
 
 create table users (
-	id int primary key
+	id integer primary key
 );
 
 create table segments (
-    id serial primary key,
-    name varchar
+    id integer primary key generated always as identity,
+    name varchar,
+    unique (name)
 );
 
 create table user_segment (
-	id serial primary key,
+	id integer primary key generated always as identity,
 	user_id bigint,
 	segment_id bigint,
     constraint fk_user
         foreign key (user_id)
-            references users (id),
+            references users (id)
+                on delete restrict
+                on update restrict,
     constraint fk_segment
         foreign key (segment_id)
             references segments (id)
+                on delete restrict
+                on update restrict,
+    unique (user_id, segment_id)
 );

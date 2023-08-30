38ef5ed9834813124ac63b0b1d8514daf2ee2aa4
ilyayashkin <drum1lord@gmail.com>
2023-08-30 19:50:57 +0800

    not nulls

--- init.sql
+++ init.sql
@@ -31,17 +31,17 @@ create table user_segment (
 
 create table user_segment_log (
     id integer primary key generated always as identity,
-    user_id integer,
-    segment_name varchar,
-    operation varchar,
+    user_id integer not null,
+    segment_name varchar not null,
+    operation varchar not null,
     operation_timestamp timestamp
 );
 
 create table segment_percentage (
     id integer primary key generated always as identity,
     segment_id integer not null,
-    user_percentage decimal(5,2) check (user_percentage <= 100 and user_percentage >= 0.01),
-    user_counter decimal(7,2) default 0,
+    user_percentage decimal(5,2) check (user_percentage <= 100 and user_percentage >= 0.01) not null,
+    user_counter decimal(7,2) default 0 not null,
     constraint fk_segment
         foreign key (segment_id)
             references segments (id)
